7cdbfda50203a9e942a5c414bc48951488c4d76c
deceivingbasher <kevin.stockmar@gmail.com>
2024-09-30 15:48:25 -0500

    Added support for additional file patterns

--- file-renaming.py
+++ file-renaming.py
@@ -1,36 +1,85 @@
 #!/usr/bin/python3
-import string
-import os
+from sys import argv,exit
+from re import search
+from os import rename, listdir, chdir
 
-def file_rename(files,alphadict):
+class InvalidNumber(Exception):
+    "Raised when a file has a number greater than 26"
+    pass
+
+def file_rename(files, path):
+    chdir(path)
     for file in files:
-        file1 = file.split(' ')
-        #get the base file name
-        base = file1[0]
-        file2 = file1[1].split('_')
-        #get the name and number that will be used
-        name = file2[0]
-        num = int(file2[1])
-        #create the new filename pattern
-        base = base + alphadict[num]
-        new = '{} {}'.format(base,name)
-        os.rename(file,new)
+        try:
+            if search("^[0-9]{3}\s[a-zA-Z]*_[0-9]{1,2}",file) != None:
+                file1 = file.split(' ')
+                #get the base file name
+                base = file1[0]
+                #get the name and number that will be used
+                name = file1[1].split('_')[0]
+                num = int(file1[1].split('_')[1].split('.')[0])
+                ext = file1[1].split('_')[1].split('.')[1]
+                #create the new filename pattern
+                if num < 1 or num > 26:
+                    raise InvalidNumber
+                new = '{}{} {}.{}'.format(base,chr(num + 96),name,ext)
+                print('Renaming file from {} to {}'.format(file,new))
+                #rename the file
+                rename(file,new)
+            elif search("^[0-9]{3}\s[0-9]{3}\-[0-9]{3}\s[a-zA-Z]*_[0-9]{1,2}",file) != None:
+                file1 = file.split(' ')
+                #get the base file name
+                base = file1[0]
+                #store the middle part for later use
+                middle = file1[1]
+                #get the name and number that will be used
+                name = file1[2].split('_')[0]
+                num = int(file1[2].split('_')[1].split('.')[0])
+                ext = file1[2].split('_')[1].split('.')[1]
+                #create the new filename pattern
+                if num < 1 or num > 26:
+                    raise InvalidNumber
+                new = '{}{} {} {}.{}'.format(base,chr(num + 96),middle,name,ext)
+                print('Renaming file from {} to {}'.format(file,new))
+                #rename the file
+                rename(file,new)
+            elif search("^[0-9]{3}\s[0-9]{3}\s[a-zA-Z]*_[0-9]{1,2}",file) != None:
+                file1 = file.split(' ')
+                #get the base file name
+                base = file1[0]
+                #store the middle part for later use
+                middle = file1[1]
+                #get the name and number that will be used
+                name = file1[2].split('_')[0]
+                num = int(file1[2].split('_')[1].split('.')[0])
+                ext = file1[2].split('_')[1].split('.')[1]
+                #create the new filename pattern
+                if num < 1 or num > 26:
+                    raise InvalidNumber
+                new = '{}{} {} {}.{}'.format(base,chr(num + 96),middle,name,ext)
+                print('Renaming file from {} to {}'.format(file,new))
+                #rename the file
+                rename(file,new)
+        except IndexError:
+            print('Invalid File Format detected')
+        except InvalidNumber:
+            print("This file name, {}, has a number that returns a non-alpha character during conversion".format(file))
+        except:
+            return None
 
 def main():
-    #build the number to alpha dictionary to be used in the file rename script
-    alphanum = []
-    alpha = []
-    for i in range(1,27):
-        alphanum.append(i)
-    for i in string.ascii_lowercase:
-        alpha.append(i)
-    alphadict = dict(zip(alphanum, alpha))
+    if len(argv) == 1:
+        argv.append('.')
+    elif argv[1] == '-h':
+        print('Usage: file-renaming <path to directory>')
+        print('*If no file path is specified, the script will run on the current directory*')
+        exit(0)
     #gather a list of files in the current directory
-    files = os.listdir('.')
+    files = listdir(argv[1])
     #strip out the python files from the list
     new_files = [file for file in files if '.py' not in file]
     #rename the files
-    file_rename(new_files, alphadict)
+    file_rename(new_files, argv[1])
 
 if __name__ == main():
     main()
--- generate_files.py
+++ generate_files.py
@@ -2,8 +2,25 @@
 from random import randint,choice
 name = ['ALEX', 'KEVIN', 'ART', 'KAT', 'ASHLEY', 'LUCAS', 'RYDER']
 
-for i in range(50):
+for i in range(20):
+    base = str(randint(100,999)) + ' ' + str(randint(100,999)) + '-' + str(randint(100,999))
+    number = randint(1,26)
+    with open('{} {}_{}.wav'.format(base,choice(name),number), 'w') as file:
+        file.write('')
+
+for i in range(20):
+    base = str(randint(100,999))
+    number = randint(1,26)
+    with open('{} {}_{}.wav'.format(base,choice(name),number), 'w') as file:
+        file.write('')
+
+for i in range(10):
     base = '00' + str(randint(1,9))
     number = randint(1,26)
-    with open('{} {}_{}'.format(base,choice(name),number), 'w') as file:
+    with open('{} {}_{}.wav'.format(base,choice(name),number), 'w') as file:
         file.write('')
+
+# 178 101-102 SOFIA_01.wav	180 101-102 SOFIA_02.wav
+# 178 101-102 SOFIA_02.wav	180 101-102 SOFIA_03.wav
+# 178 101-102 SOFIA_03.wav	file-renaming-python.py
+# 178 101-102 SOFIA_04.wav
